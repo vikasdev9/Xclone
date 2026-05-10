c2f9076931b05765e3986af6d9c0c6171ab8c4a6
vikasdev9 <vikastech986@gmail.com>
2026-05-10 20:38:09 +0530

    Now it working Now

--- mobile/utils/api.ts
+++ mobile/utils/api.ts
@@ -1,10 +1,9 @@
 import axios, { AxiosInstance } from "axios";
 import { useAuth } from "@clerk/clerk-expo";
 
-const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL || "https://x-clone-rn.vercel.app/api";
-// ! 🔥 localhost api would not work on your actual physical device
+const API_BASE_URL =process.env.EXPO_PUBLIC_API_URL || "https://xclone-zeta.vercel.app/api";// ! 🔥 localhost api would not work on your actual physical device
 // const API_BASE_URL = "http://localhost:5001/api";
-
+console.log("Testing API URL:", process.env.EXPO_PUBLIC_API_URL);
 // this will basically create an authenticated api, pass the token into our headers
 export const createApiClient = (getToken: () => Promise<string | null>): AxiosInstance => {
   const api = axios.create({ baseURL: API_BASE_URL });
